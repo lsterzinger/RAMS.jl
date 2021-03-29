23d8080e22589048fadfa075a21e13e24331505f
Lucas Sterzinger <lsterzinger@ucdavis.edu>
2021-03-29 13:45:46 -0700

    Added docs

---
+++ src/docs/make.jl
@@ -0,0 +1,9 @@
+using Documenter, RAMS
+
+makedocs(
+    sitename="RAMS.jl",
+    pages = [
+        "Index" => "index.md",
+        "API Reference" => "apiref.md"
+    ]
+)
---
+++ src/docs/src/apiref.md
@@ -0,0 +1,5 @@
+# Reference
+This lists all functions included in the `RAMS.jl` module:
+```@autodocs
+    Modules = [RAMS]
+```
